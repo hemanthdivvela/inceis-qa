77a42dd7ba52798dfee6d41475f5dc10bebd90dc
hemanthdivvela <hemanthdivvela7@gmail.com>
2026-08-29 14:38:06 +0530

    new yaml updates2

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,20 +2,9 @@ import { defineConfig, devices } from '@playwright/test';
 import { config as dotenvConfig } from 'dotenv';
 import path from 'path';
 
-// Load environment variables from .env file
+
 dotenvConfig({ path: path.resolve(__dirname, '.env') });
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
-
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
 export default defineConfig({
   testDir: './tests',
   testMatch: '**/*.spec.ts',
@@ -40,7 +29,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     // Base URL to use in actions like `await page.goto('')`. 
-    baseURL: process.env.BASE_URL,
+    baseURL: process.env.APP_URL,
     actionTimeout: 70000,   // click, fill, etc.
     navigationTimeout: 30000, // goto, reload, etc.
     //viewport: { width: 1280, height: 720 }, // Fixed size
